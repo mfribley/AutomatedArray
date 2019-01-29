e240e3480bf544126dda3a6332b52d560ce2c77b
mfribley <mfribley@mail.usf.edu>
2019-01-28 20:57:12 -0500

    Add project files.

---
+++ AutomatedArray/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutomatedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
